44c89e7870965ac33ee4488383d68e45a12e2698
mattsitek <matthewsitek@gmail.com>
2025-08-31 12:02:11 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Image from "next/image"
 import Link from "next/link"
 import { getHomepageContent } from "@/lib/datocms"
 import type { Metadata } from "next"
-import AmIReady from './am-i-ready/page'
+
 
 export const metadata: Metadata = {
   title: "Acquire & Build - Buy a Business, Build Your Freedom",
@@ -463,14 +463,6 @@ export default async function HomePage() {
                 />
               </div>
             </div>
-          </div>
-        </div>
-      </section>
-     {/* Am I Ready SECTION - Now Embedded Component */}
-      <section className="py-16 lg:py-24 bg-white" aria-labelledby="am-i-ready-heading">
-        <div className="container mx-auto px-4">
-          <div className="max-w-6xl mx-auto">
-            <AmIReady showNavigation={false} />
           </div>
         </div>
       </section>
